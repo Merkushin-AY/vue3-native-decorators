f72190f7a2dcf1a4c9e5ded3638b1eb0966ac4f1
Alexey <alexeyyb@mail.ru>
2026-03-18 14:11:02 +0300

    Versions update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 export default defineConfig({
